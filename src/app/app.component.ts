cd61db7713288a1b08f9d573da08a6d9a50a7fe5
ccastanom <159740202+ccastanom@users.noreply.github.com>
2025-05-28 13:36:14 -0500

    Creación del portafolio 😄

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 
+// Importación de componentes personalizados
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { WhatIDoComponent } from './pages/what-i-do/what-i-do.component';
+import { InspirationComponent } from './pages/inspiration/inspiration.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    AboutMeComponent,
+    WhatIDoComponent,
+    InspirationComponent,
+    ProjectsComponent,
+    ContactComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'portafolio-ng';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    importProvidersFrom(BrowserModule)
+  ]
 };
